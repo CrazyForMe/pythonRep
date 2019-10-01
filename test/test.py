c2f55c052f4bf71f73da8610655082b79e013f9b
CrazyForMe <154860134@qq.com>
2019-10-01 18:19:06 +0800

    初次提交代码到git里

---
+++ test/test.py
@@ -0,0 +1,19 @@
+def make_averager():
+    # 创建一个列表，用来保存数值
+    nums = []
+
+    # 创建一个函数，用来计算平均值
+    def averager(n) :
+        # 将n添加到列表中
+        nums.append(n)
+        # 求平均值
+        return sum(nums)/len(nums)
+
+    return averager
+
+averager = make_averager()
+
+print(averager(10))
+print(averager(20))
+print(averager(30))
+print(averager(40))
